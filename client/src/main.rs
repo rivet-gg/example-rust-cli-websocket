2cf7d8dbb11d81b2700fcbe72a0f385551037cb2
Nathan Flurry <developer@nathanflurry.com>
2022-08-24 21:09:47 -0700

    Catch errors on server safely

--- client/src/main.rs
+++ client/src/main.rs
@@ -15,6 +15,7 @@ async fn main() -> Result<()> {
 
     let rivet_client_token = std::env::var("RIVET_CLIENT_TOKEN")?;
 
+    // Build client
     let raw_client = rivet_matchmaker::Builder::dyn_https()
         .middleware(tower::layer::util::Identity::new())
         .sleep_impl(None)
@@ -25,9 +26,14 @@ async fn main() -> Result<()> {
         .build();
     let mm_api = rivet_matchmaker::Client::with_config(raw_client, config);
 
+    // Find lobby
     println!("Finding lobby");
     let lobby_res = mm_api.find_lobby().game_modes("default").send().await?;
     let lobby = lobby_res.lobby().context("lobby_res.lobby")?;
+    let player_token = lobby
+        .player()
+        .and_then(|x| x.token())
+        .context("lobby.player.token")?;
     let port = lobby
         .ports()
         .and_then(|x| x.get("default"))
@@ -40,20 +46,23 @@ async fn main() -> Result<()> {
     };
     let url = format!("{proto}://{host}");
 
-    println!("Connecting to {}", url);
+    // Connect ot server
+    println!("Connecting to {url}");
     let (ws_stream, _) = tokio_tungstenite::connect_async(url)
         .await
         .context("failed to connect")?;
+    let (mut write, read) = ws_stream.split();
     println!("Connected");
-    let (write, read) = ws_stream.split();
 
-    // Read stdin to socket
+    // Send player token
+    println!("Sending player token {player_token}");
+    write.send(Message::text(player_token)).await?;
+
+    // Build input/ouptut futures
     let read_stdin_fut = read_stdin(write);
-
-    // Write response to stdout
     let write_stdout_fut = write_stdout(read);
 
-    // Join futures
+    // Wait for input to close or socket to close
     futures_util::pin_mut!(read_stdin_fut, write_stdout_fut);
     match futures_util::future::try_select(read_stdin_fut, write_stdout_fut).await {
         Ok(_) => Ok(()),
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,18 @@
+use std::net::SocketAddr;
+
 use anyhow::{Context, Result};
-use futures_util::{future, StreamExt, TryStreamExt};
+use futures_util::{
+    future,
+    stream::{SplitSink, SplitStream},
+    SinkExt, StreamExt, TryStreamExt,
+};
 use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::{tungstenite::Message, WebSocketStream};
+
+pub type MatchmakerApi =
+    rivet_matchmaker::Client<aws_smithy_client::erase::DynConnector, tower::layer::util::Identity>;
+
+type MyWebSocketStream = WebSocketStream<tokio::net::TcpStream>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -12,6 +24,7 @@ async fn main() -> Result<()> {
         .and_then(|x| x.parse::<u16>().ok())
         .unwrap_or(5000);
 
+    // Build client
     let raw_client = rivet_matchmaker::Builder::dyn_https()
         .middleware(tower::layer::util::Identity::new())
         .sleep_impl(None)
@@ -20,37 +33,82 @@ async fn main() -> Result<()> {
         .set_uri("https://matchmaker.api.rivet.gg/v1")
         .set_bearer_token(rivet_lobby_token)
         .build();
-
     let mm_api = rivet_matchmaker::Client::with_config(raw_client, config);
-
-    mm_api.lobby_ready().send().await?;
-    println!("Lobby ready");
 
     // Create the event loop and TCP listener we'll accept connections on.
     let listener = TcpListener::bind(("0.0.0.0", port)).await?;
     println!("Listening on {}", port);
 
-    while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(accept_connection(stream));
-    }
+    // Flag server as ready to accept connections
+    mm_api.lobby_ready().send().await?;
+    println!("Lobby ready");
 
-    Ok(())
+    // Start runtime
+    loop {
+        let (stream, _) = listener.accept().await?;
+        tokio::spawn(accept_connection(mm_api.clone(), stream));
+    }
 }
 
-async fn accept_connection(stream: TcpStream) -> Result<()> {
+async fn accept_connection(mm_api: MatchmakerApi, stream: TcpStream) {
+    match accept_connection_inner(mm_api, stream).await {
+        Ok(()) => {}
+        Err(err) => println!("Connection error: {}", err),
+    }
+}
+
+async fn accept_connection_inner(mm_api: MatchmakerApi, stream: TcpStream) -> Result<()> {
     let addr = stream.peer_addr()?;
     let ws_stream = tokio_tungstenite::accept_async(stream).await?;
 
     println!("{addr}: Connected");
 
-    let (write, read) = ws_stream.split();
-    // We should not forward messages other than text or binary.
-    read.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary()))
-        .inspect(|msg| println!("{addr}: {msg:?}"))
-        .forward(write)
+    let (mut write, mut read) = ws_stream.split();
+
+    // Read init message
+    println!("{addr}: Waiting for player token");
+    let player_token = read
+        .try_next()
+        .await?
+        .context("player token not provided")?
+        .into_text()
+        .context("player token not text")?;
+
+    println!("{addr}: Verifying player token {player_token}");
+    mm_api
+        .player_connected()
+        .player_token(&player_token)
+        .send()
+        .await?;
+    println!("{addr}: Player verified");
+
+    // Read messages
+    //
+    // Gracefully catch errors so `player_disconnected` is called regardless of
+    // the result.
+    let read_res = read_messages(&addr, &mut write, &mut read).await;
+
+    println!("{addr}: Removing player from matchmaker");
+    mm_api
+        .player_disconnected()
+        .player_token(&player_token)
+        .send()
         .await?;
 
     println!("{addr}: Disconnected");
+
+    read_res
+}
+
+async fn read_messages(
+    addr: &SocketAddr,
+    write: &mut SplitSink<MyWebSocketStream, Message>,
+    read: &mut SplitStream<MyWebSocketStream>,
+) -> Result<()> {
+    while let Some(msg) = read.try_next().await? {
+        println!("{addr}: {msg:?}");
+        write.send(msg).await?;
+    }
 
     Ok(())
 }
